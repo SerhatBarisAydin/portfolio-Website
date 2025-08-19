b337281436438bec967c532612f2d2be1f059951
Barış AYDİN <serhatbarisaydin@gmail.com>
2025-08-19 19:23:10 +0300

    text fixed

--- src/components/sections/AboutMeComponent.tsx
+++ src/components/sections/AboutMeComponent.tsx
@@ -6,8 +6,8 @@ const AboutMeComponent = () => {
     <div id="about" className="hero mt-6 pt-16 h-4/5 w-12/12 md:10/12 mb-10 md:mb-0  ">
       <div className="hero-content text-center">
         <div className="w-full  ">
-          <Lottie className="md:h-50 md:mb-10 h-20" animationData={avatar} />
-          <h1 className="md:text-5xl text-3xl font-bold text-violet-500">Serhat Barış Aydin</h1>
+          <Lottie className="md:h-50 md:mb-10 h-50 mb-5" animationData={avatar} />
+          <h1 className="md:text-5xl text-3xl font-extrabold mb-3 xl:mb-0 text-violet-500">Serhat Barış Aydin</h1>
           <p className="py-3 md:py-6 md:text-lg text-xs md:text-center text-justify text-gray-500">
             I'm a computer engineer who takes an active role in a project's
             entire journey, not just the coding. I blend my expertise in
